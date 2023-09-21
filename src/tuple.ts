e15e93fc22f373c10ac7fe8cb260e52fde15f895
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-09-22 01:28:35 +0600

    tuple type in typeScript

--- src/tuple.ts
+++ src/tuple.ts
@@ -1,2 +1,10 @@
 // Declare a tuple type
 let x: [string, number] = ['hasan', 10];
+
+
+// Declare a tuple type
+let y: [string, number];
+// Initialize it
+y = ["hello", 10]; //* OK
+// Initialize it incorrectly
+y = [10, "hello"]; //! Error
