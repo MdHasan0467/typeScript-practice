c98ca813ff3f9f5cd9d2d3ec57582cc12820e8b4
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-09-23 11:37:11 +0600

    alias type in ts

---
+++ src/alias.ts
@@ -0,0 +1,20 @@
+//! Type declare er somoy always naming convention first letter Capital letter hobe
+type PersonType = {
+    name?: string;
+    age?: number;
+    profession?: string;
+    address?: string;
+}
+
+const person: PersonType = {
+    name: 'John',
+    profession: 'teacher',
+    address: 'New York'
+}
+
+const person2: PersonType = {
+    name: 'Dale',
+    age: 36,
+    profession: 'worker',
+    address: 'Dubai'
+}
