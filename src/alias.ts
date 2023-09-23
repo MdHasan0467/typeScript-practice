9c118eab14954ed71782f1aaa6c5986eb2b9125b
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-09-23 11:57:54 +0600

    func alias in ts

--- src/alias.ts
+++ src/alias.ts
@@ -26,3 +26,19 @@ const isPersonMarried: PersonMarriedType = false;
 
 type CourseType = string;
 const courseName: CourseType = 'TypeScript';
+
+
+type OperationType = (x:number, y:number)=> number;
+
+const calculate = (
+    num1 : number, //? 10
+    num2 : number, //? 20
+    operation : OperationType //? (x,y) => x+y  ||  (10,20)=> 10+20
+    ) => {
+    return operation(num1, num2);
+}
+
+calculate(10,20, (x,y)=> x+y)
+calculate(10,20, (x,y)=> x-y)
+calculate(10,20, (x,y)=> x*y)
+calculate(10,20, (x,y)=> x/y)
