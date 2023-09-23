57c167a2b8246930ebb66b510d32041256eae352
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-09-23 11:58:14 +0600

    func alias in ts

--- dist/alias.js
+++ dist/alias.js
@@ -10,3 +10,15 @@ const person2 = {
     profession: 'worker',
     address: 'Dubai'
 };
+const isPersonMarried = false;
+const courseName = 'TypeScript';
+const calculate = (num1, //? 10
+num2, //? 20
+operation //? (x,y) => x+y  ||  (10,20)=> 10+20
+) => {
+    return operation(num1, num2);
+};
+calculate(10, 20, (x, y) => x + y);
+calculate(10, 20, (x, y) => x - y);
+calculate(10, 20, (x, y) => x * y);
+calculate(10, 20, (x, y) => x / y);
